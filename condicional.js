2f8525dc8bbaa9edbaf266f8352d8ae3a0adf030
MartiFerr <136343935+MartiFerr@users.noreply.github.com>
2023-10-25 20:52:35 -0300

    cc

---
+++ condicional.js
@@ -0,0 +1,16 @@
+let nombrealumno = prompt ('¿Como es tu nombre?')
+alert ('Bienvenido/a ' +  nombrealumno )
+
+
+function mayor{
+
+
+    edaddelapersona = prompt ('¿Como es su fecha de nacimiento?')  
+    if ( edaddelapersona >=18)
+{
+    alert ('Tu edad es mayor o igual a diciocho')
+}else {
+    ('Tu edad es menos de diciocho')
+}
+
+}
